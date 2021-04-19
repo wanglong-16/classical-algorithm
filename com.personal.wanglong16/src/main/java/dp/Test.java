0b47309cead8ba69eeb3b8dcadc6d09b6e7d7235
wanglong16 <wanglong16@meicai.cn>
2021-04-19 08:31:44 +0800

    dp

---
+++ com.personal.wanglong16/src/main/java/dp/Test.java
@@ -0,0 +1,15 @@
+package dp;
+
+/**
+ * @description:
+ * @version: 1.0
+ * @date: 2021-04-18 10:13:52
+ * @author: wanglong16@meicai.cn
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        Practise practise = new Practise();
+        practise.lengthOfLIS(new int[] {4,10,4,3,8,9});
+    }
+}
